193468b3eac87b45ce810deb2236a20ec603733e
Maxwell H. Muir <maxwellh.muir@cubic.com>
2018-02-27 10:53:05 -0800

    formatted source code

--- src/test/java/com/cubic/nisjava/tests/nextlink/productcatalog/ProductCatalogNewTests.java
+++ src/test/java/com/cubic/nisjava/tests/nextlink/productcatalog/ProductCatalogNewTests.java
@@ -24,34 +24,38 @@ import com.cubic.nisjava.apiobjects.WSStoredValue;
 public class ProductCatalogNewTests extends RESTEngine {
 
 	private final Logger LOG = Logger.getLogger(this.getClass().getName());
-	
+
 	/**
 	 * Get the New Product Catalog.
 	 * 
-	 * @param context  The TestNG context object
-	 * @param data  The test data
-	 * @throws Throwable  Thrown if something goes wrong
+	 * @param context
+	 *            The TestNG context object
+	 * @param data
+	 *            The test data
+	 * @throws Throwable
+	 *             Thrown if something goes wrong
 	 */
 	@Test(dataProvider = AppConstants.DATA_PROVIDER, dataProviderClass = NISDataProviderNEXTLINK_v1.class)
 	public void getNewProductCatalog(ITestContext context, Hashtable<String, String> data) throws Throwable {
 		String testCaseName = data.get("TestCase_Description");
 		LOG.info("TestCase_Description=" + testCaseName);
-		
+
 		try {
 			LOG.info("##### Setting up automation test");
 			RESTActions restActions = setupAutomationTest(context, testCaseName);
-			
+
 			LOG.info("##### Getting REST Headers");
-			Hashtable<String,String> headers = BackOfficeUtils.createRESTHeader(RESTConstants.APPLICATION_JSON);
-			
+			Hashtable<String, String> headers = BackOfficeUtils.createRESTHeader(RESTConstants.APPLICATION_JSON);
+
 			String newProdCatFmt = data.get("PRODUCT_CATALOG_URL");
-			String newProdCatURL = String.format( newProdCatFmt, data.get("HOST"), data.get("PORT") );
-			
+			String newProdCatURL = String.format(newProdCatFmt, data.get("HOST"), data.get("PORT"));
+
 			Hashtable<String, String> urlQueryParameters = new Hashtable<String, String>();
-			
+
 			LOG.info("##### Calling Product Catalog URL");
-			ClientResponse clientResponse = restActions.getClientResponse(newProdCatURL, headers, urlQueryParameters, RESTConstants.APPLICATION_JSON);
-			
+			ClientResponse clientResponse = restActions.getClientResponse(newProdCatURL, headers, urlQueryParameters,
+					RESTConstants.APPLICATION_JSON);
+
 			// Verify HTTP response code
 			int status = clientResponse.getStatus();
 			String msg = "WRONG HTTP RESPONSE CODE - EXPECTED 200, FOUND " + status;
@@ -60,78 +64,66 @@ public class ProductCatalogNewTests extends RESTEngine {
 			String response = clientResponse.getEntity(String.class);
 			LOG.info("##### Listing response...");
 			LOG.info(response);
-			
+
 			Gson gson = new Gson();
-			
+
 			LOG.info("##### Parsing Catalog Response");
 			WSCatalogContainer catResponse = gson.fromJson(response, WSCatalogContainer.class);
-			
+
 			LOG.info("##### Testing top-level items");
-			restActions.assertTrue("Successful".equals(catResponse.getHdr().getResult()), 
+			restActions.assertTrue("Successful".equals(catResponse.getHdr().getResult()),
 					"HDR RESULT IS NOT 'Successful' BUT IT SHOULD BE");
-				
-			restActions.assertTrue(null != catResponse.getHdr().getUid(), 
-					"HDR RESULT IS null BUT IT SHOULD NOT BE");
-			
-			restActions.assertTrue(catResponse.getCatalog().getMaxAddValue() != null, 
-					"MAX ADD VALUE IS NULL BUT IT SHOULD NOT BE" );
-			
-			restActions.assertTrue(catResponse.getCatalog().getMaxAddValue() > 0, 
-					"MAX ADD VALUE IS =< ZERO BUT IT SHOULD NOT BE" );
-			
+
+			restActions.assertTrue(null != catResponse.getHdr().getUid(), "HDR RESULT IS null BUT IT SHOULD NOT BE");
+
+			restActions.assertTrue(catResponse.getCatalog().getMaxAddValue() != null,
+					"MAX ADD VALUE IS NULL BUT IT SHOULD NOT BE");
+
+			restActions.assertTrue(catResponse.getCatalog().getMaxAddValue() > 0,
+					"MAX ADD VALUE IS =< ZERO BUT IT SHOULD NOT BE");
+
 			LOG.info("##### Testing stored value list");
 			List<WSStoredValue> storedValueList = catResponse.getCatalog().getStoredValue();
-			for ( WSStoredValue storedValue : storedValueList ) {
-				restActions.assertTrue(storedValue.getName() != null, 
-					"STORED VALUE IS NULL BUT IT SHOULD NOT BE" );
-				
-				restActions.assertTrue(storedValue.getAmount() != null, 
-						"AMOUNT IS NULL BUT IT SHOULD NOT BE" );
-				
-				restActions.assertTrue(storedValue.getAmount() > 0, 
-						"AMOUNT IS =< ZERO BUT IT SHOULD NOT BE" );	
-				
-				List<String> loadTypeList = storedValue.getLoadType();	
-				if ( null != loadTypeList ) {
-					restActions.assertTrue( loadTypeList.size() == 0,
-						"LOAD TYPE LIST HAS SIZE > 0 BUT IT SHOULD NOT" );
+			for (WSStoredValue storedValue : storedValueList) {
+				restActions.assertTrue(storedValue.getName() != null, "STORED VALUE IS NULL BUT IT SHOULD NOT BE");
+
+				restActions.assertTrue(storedValue.getAmount() != null, "AMOUNT IS NULL BUT IT SHOULD NOT BE");
+
+				restActions.assertTrue(storedValue.getAmount() > 0, "AMOUNT IS =< ZERO BUT IT SHOULD NOT BE");
+
+				List<String> loadTypeList = storedValue.getLoadType();
+				if (null != loadTypeList) {
+					restActions.assertTrue(loadTypeList.size() == 0, "LOAD TYPE LIST HAS SIZE > 0 BUT IT SHOULD NOT");
 				}
 			}
-			
+
 			LOG.info("##### Testing Catalog Products");
-			for ( WSProduct product : catResponse.getCatalog().getProduct() ) {
-				restActions.assertTrue(product.getFareInstrumentId() != null, 
-						"FARE INSTRUMENT ID IS NULL BUT IT SHOULD NOT BE" );
-				
-				restActions.assertTrue(product.getFareInstrumentId() > 0, 
-						"FARE INSTRUMENT ID IS =< ZERO BUT IT SHOULD NOT BE" );
-				
-				restActions.assertTrue(product.getName() != null, 
-						"NAME IS NULL BUT IT SHOULD NOT BE" );
-				
-				restActions.assertTrue(product.getName().length() > 0, 
-						"NAME IS NULL BUT IT SHOULD NOT BE" );
-				
-				restActions.assertTrue(product.getNameShort() != null, 
-						"NAME SHORT IS NULL BUT IT SHOULD NOT BE" );
-				
-				restActions.assertTrue(product.getNameShort().length() > 0, 
-						"NAME SHORT IS NULL BUT IT SHOULD NOT BE" );
-				
-				restActions.assertTrue(product.getSupportsAutoload() != null, 
-						"SUPPORTS AUTOLOAD IS NULL BUT IT SHOULD NOT BE" );
-				
-				restActions.assertTrue(product.getSupportsAutoload() == false, 
-						"SUPPORTS AUTOLOAD IS NOT FALSE BUT IT SHOULD BE" );
-				
-				restActions.assertTrue(product.getPrice() != null, 
-						"PRICE IS NULL BUT IT SHOULD NOT BE" );
-				
-				restActions.assertTrue(product.getPrice() > 0, 
-						"PRICE IS =< ZERO BUT IT SHOULD NOT BE" );
+			for (WSProduct product : catResponse.getCatalog().getProduct()) {
+				restActions.assertTrue(product.getFareInstrumentId() != null,
+						"FARE INSTRUMENT ID IS NULL BUT IT SHOULD NOT BE");
+
+				restActions.assertTrue(product.getFareInstrumentId() > 0,
+						"FARE INSTRUMENT ID IS =< ZERO BUT IT SHOULD NOT BE");
+
+				restActions.assertTrue(product.getName() != null, "NAME IS NULL BUT IT SHOULD NOT BE");
+
+				restActions.assertTrue(product.getName().length() > 0, "NAME IS NULL BUT IT SHOULD NOT BE");
+
+				restActions.assertTrue(product.getNameShort() != null, "NAME SHORT IS NULL BUT IT SHOULD NOT BE");
+
+				restActions.assertTrue(product.getNameShort().length() > 0, "NAME SHORT IS NULL BUT IT SHOULD NOT BE");
+
+				restActions.assertTrue(product.getSupportsAutoload() != null,
+						"SUPPORTS AUTOLOAD IS NULL BUT IT SHOULD NOT BE");
+
+				restActions.assertTrue(product.getSupportsAutoload() == false,
+						"SUPPORTS AUTOLOAD IS NOT FALSE BUT IT SHOULD BE");
+
+				restActions.assertTrue(product.getPrice() != null, "PRICE IS NULL BUT IT SHOULD NOT BE");
+
+				restActions.assertTrue(product.getPrice() > 0, "PRICE IS =< ZERO BUT IT SHOULD NOT BE");
 			}
-		}
-		finally {
+		} finally {
 			teardownAutomationTest(context, testCaseName);
 			LOG.info("##### Done!");
 		}
