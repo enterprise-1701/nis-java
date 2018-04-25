80e734cd227276de738530a8908f8dbfe6a5a9f9
Kevin Kraft <kevin.kraft@cubic.com>
2018-04-25 13:54:45 -0700

    Added additional Data Provider methods to be more dynamic with the framework for how the json input files are read and handled.

--- src/main/java/com/cubic/nisjava/dataproviders/NISDataProviderSource.java
+++ src/main/java/com/cubic/nisjava/dataproviders/NISDataProviderSource.java
@@ -9,11 +9,37 @@ import com.cubic.nisjava.constants.*;
 
 public class NISDataProviderSource {
 	
+	static String testDatafilePath;
+	static String parentElement;
+	
 	@DataProvider(name = AppConstants.DATA_PROVIDER)
     public static Object[][] dataProvider(Method method) throws Throwable {
-
-        String testDatafilePath = AppConstants.NIS_TEST_DATA_FOLDER + "/" + method.getDeclaringClass().getSimpleName() + ".json";
-        String parentElement = method.getName();
+        generateData();
         return TestDataUtil.getTestDataFromJson(testDatafilePath, parentElement);
     }
+	
+	// ADD DATA PROVIDER TO RETURN JSON WITH ARRAYS AND/OR WITHOUT TRIMMING INPUT DATA
+	@DataProvider(name = AppConstants.DATA_PROVIDER_RETURN_ARRAYS_NO_TRIM)
+	public static Object[][] dataProviderReturnArraysNoTrim (Method method) throws Throwable {
+		generateData();
+		return TestDataUtil.getTestDataFromJsonFile(testDatafilePath, parentElement, true, true);
+	}
+	
+	@DataProvider(name = AppConstants.DATA_PROVIDER_RETURN_ARRAYS)
+	public static Object[][] dataProviderReturnArrays (Method method) throws Throwable {
+		generateData();
+        return TestDataUtil.getTestDataFromJsonFile(testDatafilePath, parentElement, true, false);
+	}
+	
+	@DataProvider(name = AppConstants.DATA_PROVIDER_NO_TRIM)
+	public static Object[][] dataProviderNoTrim (Method method) throws Throwable {
+		generateData();
+        return TestDataUtil.getTestDataFromJsonFile(testDatafilePath, parentElement, false, true);
+	}
+	
+	public static void generateData() {
+		testDatafilePath = AppConstants.NIS_TEST_DATA_FOLDER + "/" + method.getDeclaringClass().getSimpleName() + ".json";
+        parentElement = method.getName();
+	}
+	
 }
