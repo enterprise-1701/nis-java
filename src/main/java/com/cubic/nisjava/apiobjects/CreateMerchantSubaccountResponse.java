85eed3a98ea8ee2f716279b6ab005436acba2264
vijayabhaskar <Vijaya.BhaskarPalem@cubic.com>
2018-06-12 17:30:05 +0530

    Added createdmerchant test cases

---
+++ src/main/java/com/cubic/nisjava/apiobjects/CreateMerchantSubaccountResponse.java
@@ -0,0 +1,24 @@
+package com.cubic.nisjava.apiobjects;
+
+public class CreateMerchantSubaccountResponse {
+	
+	private String userId;
+	private WSHdr hdr;
+
+	public WSHdr getHdr() {
+		return hdr;
+	}
+
+	public void setHdr(WSHdr hdr) {
+		this.hdr = hdr;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+}
